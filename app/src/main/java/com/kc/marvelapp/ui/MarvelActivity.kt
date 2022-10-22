3f40e397d7813997994f3cc430b15d999232782c
kunal-ch <kunalch36@gmail.com>
2022-10-22 13:03:03 +0530

    feat: new project

---
+++ app/src/main/java/com/kc/marvelapp/ui/MarvelActivity.kt
@@ -0,0 +1,12 @@
+package com.kc.marvelapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kc.marvelapp.R
+
+class MarvelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marvel)
+    }
+}
