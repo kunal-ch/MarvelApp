dc69fe7846615a1d755860f4b93236e63d7b84a6
kunal-ch <kunalch36@gmail.com>
2022-10-27 12:24:26 +0530

    fix : Character Image

--- app/src/main/java/com/kc/marvelapp/presentation/character_listing/CharacterItem.kt
+++ app/src/main/java/com/kc/marvelapp/presentation/character_listing/CharacterItem.kt
@@ -1,11 +1,14 @@
 package com.kc.marvelapp.presentation.character_listing
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -13,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kc.marvelapp.domain.models.ComicCharacter
 import com.kc.marvelapp.util.Utils
+import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun CharacterItem(
@@ -23,6 +27,16 @@ fun CharacterItem(
     modifier = modifier,
     verticalAlignment = Alignment.CenterVertically
     ) {
+        CoilImage(
+            imageModel = character.thumbnail.path+"."+character.thumbnail.extension,
+            // Crop, Fit, Inside, FillHeight, FillWidth, None
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
         Column(
             modifier = Modifier.weight(1f)
         ) {
